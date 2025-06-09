2a533e3fac6fa3ec4d95c49436e187817fb65d0b
M-Davy <mmutukudavis@gmail.com>
2025-06-09 10:50:31 +0300

    Second commit

---
+++ __palindromes_.py
@@ -0,0 +1,75 @@
+class Node:
+
+    def init(self,data):
+
+        #constructor : initializes and declares variables and attributes
+
+        self.data=data
+
+
+
+        self.next=None
+
+
+
+class LinkedList:
+
+    def init(self):
+
+        self.head=None
+
+
+
+
+
+    def insertAtTheBeginning(self,new_data):
+
+        new_node=Node(new_data)
+
+        new_node.next = self.head
+
+        self.head = new_node
+
+
+
+
+
+    def printList(self):
+
+        temp = self.head
+
+
+
+        while temp:
+
+            print(f"{temp.data} ", end='')
+
+            temp = temp.next
+
+
+
+        print()
+
+
+
+
+
+if __name__=='main':
+
+    llist = LinkedList()
+
+
+
+    llist.insertAtTheBeginning("New")
+
+    llist.insertAtTheBeginning("Data")
+
+    llist.insertAtTheBeginning("Is")
+
+    llist.insertAtTheBeginning("New")
+
+    llist.insertAtTheBeginning("Data") #the last one runs first
+
+
+
+    llist.printList()
